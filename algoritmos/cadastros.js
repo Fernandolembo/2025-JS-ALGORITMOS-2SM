1002cc84732aa924e40ee3932242be98233d00ab
fernando Lembo <Fernandolembo33@hotmail.com>
2025-09-03 20:13:14 -0300

    Primeiras atualizações

---
+++ algoritmos/cadastros.js
@@ -0,0 +1,106 @@
+	
+
+		escreva ("Entre com a placa: ")
+
+		leia (placa)
+
+		escreva ("Entre com o modelo: ")
+
+		leia (modelo)
+
+		escreva ("Entre com a cor: ")
+
+		leia (cor)
+
+		escreva ("Entre com o número da vaga: ")
+
+		leia (vaga)
+
+		escreva ("Entre com a hora de entrada: ")
+
+		leia (horaEntrada)
+
+		escreva( "Informações: placa: "+placa+" | modelo: "+modelo+" | cor: "+cor+" hora de entrada: "+horaEntrada)
+
+	//
+
+    
+    escreva("Digite o número da matrícula: ")
+    leia(matricula)
+    
+    escreva("Digite o nome do aluno: ")
+    leia(nome)
+    
+    escreva("Digite a idade do aluno: ")
+    leia(idade)
+    
+    escreva("Digite o sexo do aluno (M/F): ")
+    leia(sexo)
+    
+    escreva("Digite o celular do aluno: ")
+    leia(celular)
+    
+    escreva("Digite o e-mail do aluno: ")
+    leia(email)
+    
+ 
+    escreva("Cadastro do Aluno\n")
+    escreva("Matrícula: ", matricula, "\n")
+    escreva("Nome: ", nome, "\n")
+    escreva("Idade: ", idade, "\n")
+    escreva("Sexo: ", sexo, "\n")
+    escreva("Celular: ", celular, "\n")
+    escreva("E-mail: ", email, "\n")
+ //
+ 
+		escreva ("Digite o nome do primeiro produto: ")
+
+		leia (produto1)
+
+		escreva ("Digite o valor do primeiro produto: ")
+
+		leia (valor1)
+ 
+		escreva ("Digite o nome do segundo produto: ")
+
+		leia (produto2)
+
+		escreva ("Digite o valor do segundo produto: ")
+
+		leia (valor2)
+ 
+		escreva ("Digite o nome do terceiro produto: ")
+
+		leia (produto3)
+
+		escreva ("Digite o valor do terceiro produto: ")
+
+		leia (valor3)
+ 
+		escreva ("Digite o nome do quarto produto: ")
+
+		leia (produto4)
+
+		escreva ("Digite o valor do quarto produto: ")
+
+		leia (valor4)
+ 
+		escreva ("Digite o nome do quinto produto: ")
+
+		leia (produto5)
+
+		escreva ("Digite o valor do quinto produto: ")
+
+		leia (valor5)
+ 
+		escreva ("Produto 1: "+produto1+" | Valor: "+valor1)
+
+		escreva (" Produto 2: "+produto1+" | Valor: "+valor2)
+
+		escreva (" Produto 3: "+produto1+" | Valor: "+valor3)
+
+		escreva (" Produto 4: "+produto1+" | Valor: "+valor4)
+
+		escreva (" Produto 5: "+produto1+" | Valor: "+valor5)
+
+		escreva(" Valor total dos produtos: "+(valor1+valor2+valor3+valor4+valor5))
---
+++ script/1att.js
@@ -0,0 +1,40 @@
+//1
+console.log("Bom dia, Mundo");
+
+//2 
+Num1 = 2; 
+console.log(Num1**3);
+
+//3
+F=50;
+c= (F-32)*0.5556;
+console.log(c);
+
+//4
+base=10;
+altura=5;
+area=(base*altura)/2;
+console.log(area);
+
+//5
+R=5;
+A=3.14*R**2;
+P=2*3.14*R;
+console.log(P);
+console.log(A);
+
+//6
+let Capital_inicial = 50;
+let taxa_juros = 0.15; // 15% = 0.15
+let tempo_mes = 6;
+
+let montante = Capital_inicial * (1 + taxa_juros) ** tempo_mes;
+console.log(montante.toFixed(2)); 
+
+
+//7
+Nota1=7;
+Nota2=8;
+Nota3=6;
+media=(Nota1+Nota2+Nota3) /3;
+console.log(media);
