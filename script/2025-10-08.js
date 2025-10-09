9350fc37cf0c7ecb8016bf58019b1dfde4ec7847
Fernando Lembo <fernandolembo33@hotmail.com>
2025-10-08 22:28:41 -0300

    Loop for

---
+++ script/2025-10-08.js
@@ -0,0 +1,25 @@
+// Aula de Loop for 
+
+for (var i = 0; i<5; i++){
+    console.log(i);
+}
+
+//1 até 50
+
+for (var a = 1; a<51; a++){
+    console.log(a);
+}
+
+//tabuada 
+
+for (var t = 0; t<=10; t++){
+    console.log (`7 x ${t} = ${7 * t}`);
+}
+
+//exibir numeros pares de 1 há 50 
+
+for (var p = 1; p <= 50; p++) {
+    if (p % 2 === 0) {
+        console.log(`Número par: ${p}`);
+    }
+}
