3004e4162b2f6449efd1500ce2446491055de5a7
Fernando lembo <fernandolembo33@hotmail.com>
2025-10-22 22:23:27 -0300

    While e Do while

---
+++ script/22-10-2025.js
@@ -0,0 +1,45 @@
+//tabuada
+
+for (var t = 0; t<=10; t++){
+    console.log (`3 x ${t} = ${3 * t}`);
+}
+
+// tabuada diferente
+
+var numero = 2
+for (var i = 0; i <= 10; i++){
+    console.log(numero +"x"+ i+"=", numero * i)
+}
+
+// while tabuada 
+
+var i = 0
+
+while (i <= 10 ) {
+  console.log(`7 x ${i} = ${7 * i}`);
+  i++;
+}
+
+// do while aumento
+
+var aumento = 5000
+
+do {
+    console.log ("O salário ainda é R$" + aumento);
+    aumento += 50;
+} while (aumento <5000);
+
+//while aumento 
+
+
+var aumento = 5000
+
+while(aumento <5000){
+console.log ("tf O salário ainda é R$" + aumento);
+aumento += 50;
+}
+
+
+
+    
+
