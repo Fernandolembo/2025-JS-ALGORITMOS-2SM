d06f9fe9315fd83df2fff58d104f736d561430a2
fernando lembo <fernandolembo33@hotmail.com>
2025-10-29 20:28:58 -0300

    array

---
+++ 29-10-2025.js
@@ -0,0 +1,26 @@
+//let vetor
+
+let vetor = [10, 20, 30, 40, 50];
+console.log(vetor[4]);
+
+//let matriz
+
+let matriz = [
+     [1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9],
+];
+
+console.log(matriz[1][1]);
+
+//nome 
+
+let nome = ["Fernando", "Letícia", "Eliana", "Luiza", "Luiz"];
+console.log(nome[4]);
+
+
+//contar elementos do array 
+
+let numeros = [10, 20, 30, 40, 50];
+
+console.log("Quantidade de elementos:", numeros.length)
